6449e938c828f9851666c88a4c4b5c39fc1e471b
pulividyavathi <puli.v@northeastern.edu>
2023-09-12 22:05:02 -0400

    Connected to Mongodb

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
  const express=require('express')
+ const mongoose=require('mongoose')
  const app=express()
 
  app.get('/',(req,res)=>{
     res.send('Hello Node Api')
  })
 
+app.get('/blog',(req,res)=>{
+    res.send('Hello Blog Vidya this side')
+})
+  
 
- app.listen(3000,()=>{
-    console.log('Node api app is running on 3000')
+ mongoose.connect
+ ('mongodb+srv://pulividyavathi:eCX8uByWb7xQTlhv@vidyaapi.jwtnehi.mongodb.net/Node-API?retryWrites=true&w=majority')
+ .then(()=>{
+    console.log('Connected to Mongodb')
+    app.listen(3000,()=>{
+        console.log('Node api app is running on 3000')
+     })
+ }).catch((error)=>{
+    console.log(error)
  })
